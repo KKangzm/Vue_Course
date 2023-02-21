9267f665140707f0c660863bbe6bed157d56d0d2
KKangzm <1473255158@qq.com>
2023-02-21 11:35:11 +0800

    props

--- 05_练习/src/App.vue
+++ 05_练习/src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
-    import {ref} from "vue"
+    import {ref, reactive} from "vue"
+    import TabItem from "./components/TabItem.vue"
 
     // 创建一个变量来记录选项卡的状态
     const current = ref(0)  // 0 表示吃的， 1 表示用的
+
+    const food = reactive({
+        name: "蛋糕甜品",
+        img: "/images/蛋糕甜品.png",
+        rate: 1,
+        price: "300 刀勒"
+    })
+
+    const goods = reactive({
+        name: "个护清洁",
+        img: "/images/个护清洁.png",
+        rate: 1,
+        price: "200 刀勒"
+    })
 </script>
 
 <template>
@@ -30,41 +45,13 @@
             <div v-show="current === 0">
                 <!-- 吃的 -->
                 <div class="tab-list">
-                    <div class="tab-item">
-                        <!-- 图片 -->
-                        <div class="photo">
-                            <img src="/images/蛋糕甜品.png" alt="蛋糕甜品">
-                            <span>1</span>
-                        </div>
-
-                        <!-- 描述 -->
-                        <div class="desc">
-                            <span class="name">蛋糕甜品</span>
-                            <div class="hot-bar">
-                                <div class="inner">300 刀勒</div>
-                            </div>
-                        </div>
-                    </div>
+                    <TabItem :item="food"></TabItem>
                 </div>
             </div>
             <div v-show="current === 1">
                 <!-- 用的 -->
                 <div class="tab-list">
-                    <div class="tab-item">
-                        <!-- 图片 -->
-                        <div class="photo">
-                            <img src="/images/个护清洁.png" alt="个护清洁">
-                            <span>1</span>
-                        </div>
-
-                        <!-- 描述 -->
-                        <div class="desc">
-                            <span class="name">个护清洁</span>
-                            <div class="hot-bar">
-                                <div class="inner">200 刀勒</div>
-                            </div>
-                        </div>
-                    </div>
+                    <TabItem :item="goods"></TabItem>
                 </div>
             </div>
         </div>
@@ -106,71 +93,5 @@
 
 .tab-list {
     margin: 20px;
-}
-
-.tab-item {
-    display: flex;
-    margin-bottom: 40px;
-}
-
-/* 容器 */
-.photo {
-    width: 150px;
-    background-color: #fff;
-    border-radius: 20px;
-    overflow: hidden;
-
-    position: relative;
-}
-
-/* 图片 */
-.photo img {
-    width: 100%;
-    vertical-align: middle;
-}
-
-/* 排名 */
-.photo span {
-    position: absolute;
-    width: 50px;
-    height: 50px;
-    background-color: rgb(245, 102, 1);
-    top: 0px;
-    left: 0px;
-    font-size: 30px;
-    font-weight: bold;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-bottom-right-radius: 20px;
-    border-top-left-radius: 20px;
-}
-
-.desc {
-    display: flex;
-    flex-flow: column;
-    justify-content: space-evenly;
-    font-size: 28px;
-    color: #fff;
-    flex: auto;
-    margin-left: 30px;
-}
-
-.hot-bar {
-    background-color: rgb(3, 37, 103);
-    border-radius: 20px;
-    text-indent: 0.5em;
-    overflow: hidden;
-}
-
-.inner {
-    border-radius: 20px;
-    background-image: linear-gradient(
-    90deg, 
-    rgb(187, 3, 52) 50%, 
-    rgb(66, 2, 12));
-    width: 10%;
-    white-space: nowrap;
 }
 </style>
---
+++ 05_练习/src/components/TabItem.vue
@@ -0,0 +1,100 @@
+
+<!-- 
+    创建一个响应式对象
+    - 子组件中的数据通常不会再子组件中直接定义，这样会导致数据和视图发生耦合
+    - 子组件中的数据通常会在创建组件实例时确定！
+    - 父组件可以通过 props 将数据传递给子组件
+
+    使用props
+        - 先在子组件中定义props
+ -->
+ <script setup>
+ const props = defineProps(["item"])
+ const item = props.item
+</script>
+
+<template>
+    <div class="tab-item">
+        <!-- 图片 -->
+        <div class="photo">
+            <img :src="item.img" :alt="item.name">
+            <span>{{item.rate}}</span>
+        </div>
+
+        <!-- 描述 -->
+        <div class="desc">
+            <span class="name">{{item.name}}</span>
+            <div class="hot-bar">
+                <div class="inner">{{item.price}}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .tab-item {
+    display: flex;
+    margin-bottom: 40px;
+}
+
+/* 容器 */
+.photo {
+    width: 150px;
+    background-color: #fff;
+    border-radius: 20px;
+    overflow: hidden;
+
+    position: relative;
+}
+
+/* 图片 */
+.photo img {
+    width: 100%;
+    vertical-align: middle;
+}
+
+/* 排名 */
+.photo span {
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background-color: rgb(245, 102, 1);
+    top: 0px;
+    left: 0px;
+    font-size: 30px;
+    font-weight: bold;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom-right-radius: 20px;
+    border-top-left-radius: 20px;
+}
+
+.desc {
+    display: flex;
+    flex-flow: column;
+    justify-content: space-evenly;
+    font-size: 28px;
+    color: #fff;
+    flex: auto;
+    margin-left: 30px;
+}
+
+.hot-bar {
+    background-color: rgb(3, 37, 103);
+    border-radius: 20px;
+    text-indent: 0.5em;
+    overflow: hidden;
+}
+
+.inner {
+    border-radius: 20px;
+    background-image: linear-gradient(
+    90deg, 
+    rgb(187, 3, 52) 50%, 
+    rgb(66, 2, 12));
+    width: 50%;
+    white-space: nowrap;
+}
+</style>
